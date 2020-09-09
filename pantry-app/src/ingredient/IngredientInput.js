d629fce68f9fbf02405dd05af63991b3ce341a72
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-09-09 12:57:11 -0400

    fix input to be used by form

--- pantry-app/src/ingredient/IngredientInput.js
+++ pantry-app/src/ingredient/IngredientInput.js
@@ -3,11 +3,11 @@ import React from 'react'
 class IngredientInput extends React.Component {
     render(){
         return (
-            <div autoComplete="off">
-                {this.props.readonly ? "" : <label htmlFor={`ingredient-name-${this.props.id}`}>Name: </label>}
-                <input type="text" name="name" id={`ingredient-name-${this.props.id}`} value={this.props.ingredient.name} onChange={this.props.handleChange} autoComplete="off" readOnly={this.props.readonly}/>
+            <div>
+                <label htmlFor={`ingredient-name-${this.props.id}`}>Name: </label>
+                <input type="text" name="name" id={`ingredient-name-${this.props.id}`} value={this.props.ingredient.name} onChange={this.props.handleChange} autoComplete="off"/>
                 <input type="number" name="quantity" id={`ingredient-quantity-${this.props.id}`} value={this.props.ingredient.quantity} onChange={this.props.handleChange} style={{width: "50px"}} autoComplete="off"/>
-                {this.props.readonly ? "" : <button id={`ingredient-remove-${this.props.id}`} onClick={this.props.handleRemove}>X</button>}
+                <button id={`ingredient-remove-${this.props.id}`} onClick={this.props.handleRemove}>X</button>
             </div>
         )
     }
